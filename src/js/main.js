173a74c7313f8adf38d62fa63757e52da69a1860
Gerald Michelant <gerald.michelant@gmail.com>
2017-12-15 00:11:37 -0500

    async function for sound

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,12 @@ let greenTile= document.getElementById("3");
 let yellowTile= document.getElementById("4");
 let score = document.getElementById("score");
 let serie=[];
-let round=0;
+let playerTurn = false;
+let strictMode=false;
+let round=[0];
 let long=0;
+let color;
+let speed = 1500;
 let responses=[];
 let audio = document.getElementById("audio");
 
@@ -19,7 +23,6 @@ Array.from(touches).forEach(element => {
   element.addEventListener('click', checkSound);
   element.addEventListener('mouseout', mouseOut);
   element.addEventListener('mouseover', mouseIn);
-  console.log(element);
 });
 
 
@@ -56,9 +59,9 @@ function mouseIn(e) {
 
 
 function checkSound(tile) {
+  if(playerTurn) {
   responses.push(tile.srcElement.dataset.colorindex);
   console.log(responses);
-
 
     if (Number(responses[long]) == serie[long]) {
       audio.src = audiotrack[tile.srcElement.dataset.colorindex-1];
@@ -68,72 +71,102 @@ function checkSound(tile) {
       audio.src = wrongAnswer;
       responses=[];
       long=0;
-      }
-      var playPromise = audio.play();
-      if (playPromise !== undefined) {
-        playPromise
-        .then(_ => {
-            })
-      }
+      if ( strictMode) {
+        resetGame();
 
-  if (responses.length == round+1) {
-    responses=[];
-    console.log("woooo");
-    round+=1;
-    long=0;
-    setTimeout(function() {playSound()}, 1000);
+      }
+      else if (!strictMode){
+        setTimeout(function() {playTrack()}, 2000);
+      }
+    }
+    playSound();
+
+    if (responses.length == round.length) {
+      playerTurn=false;
+      if (round===20) {
+        score.innerHTML="WIN";
+        setTimeout(function() {startGame()}, 5000);
+      }
+      else { 
+        if (round>=13) {
+        speed=900;
+        }
+        else if (round>=9) {
+          speed=1000;
+        }
+        else if (round>=5) {
+          speed=1000;
+        }
+      responses=[];
+      console.log("woooo");
+      score.innerHTML=round.length;
+      round.push(round.length);
+      long=0;
+      setTimeout(function() {playTrack()}, 2000);
+      }
+    
+    }
   }
 }
-
 
 function startGame() {
   resetGame();
   serie = initiateSerie();
-  playSound();
 }
 
-function strictMode() {
-
-
+function strictModel() {
+  document.getElementsByClassName("btnStrict")[0].classList.toggle( "selected" );
+  strictMode = strictMode ? false : true;
 }
 
 function initiateSerie() {
+  serie=[];
   for (let i=0; i<20; i++){
     serie.push(Math.floor(Math.random() * 4) + 1)
   }
-  console.log(serie);
   return serie;
 
 }
 function resetGame() {
-  round=0
-  serie=[];
+  round=[0];
   responses=[];
+  score.innerHTML="0";
+  setTimeout(function() {playTrack()}, 2000);
 }
 
-async function playSound() {
-
-  for(let i=0; i<round+1; i++) {
-    console.log(i)
-    console.log(serie[i]);
-    setTimeout(function() {
-    audio.src = audiotrack[serie[i]-1];
-    var playPromise = audio.play();  
-      console.log(audio.src);
-      let color = document.getElementById(serie[round]).getAttribute('stroke')
-      let newColor = LightenDarkenColor(color, 60);
-      document.getElementById(serie[round]).setAttribute('stroke', newColor);
-      setTimeout(function() {document.getElementById(serie[round]).setAttribute('stroke', color)}, 1000);
-      if (playPromise !== undefined) {
-        playPromise.then(_ => {
-          
-        })
-        .catch(error => {
-          console.log("error")
-        });
-      }
-  }, 1000)};
+function playTrack() {
+  var y=0;
+  function nextTrack() {
+    audio.src = audiotrack[serie[y]-1];
+    playSound(serie[y]);
+    y++;
+    if (y<round.length) {
+      setTimeout(nextTrack, speed);
+    }
+    else if (y === round.length) {
+      playerTurn=true;
+    }
   }
+  nextTrack();
+  }
+
+function playSound(elem) {
+    var playPromise = audio.play(); 
+    
+  if (playPromise !== undefined) {
+    playPromise.then(_ => {
+      color = document.getElementById(elem).getAttribute('stroke')
+      let newColor = LightenDarkenColor(color, 60);
+      document.getElementById(elem).setAttribute('stroke', newColor);
+      setTimeout(function() {document.getElementById(elem).setAttribute('stroke', color)}, 1200);
+   
+    })
+    .catch(error => {
+
+    });
+  }
+
+}
 
 
 
@@ -147,19 +180,15 @@ function LightenDarkenColor(col, amt) {
     }
  
     var num = parseInt(col,16);
- 
     var r = (num >> 16) + amt;
- 
     if (r > 255) r = 255;
     else if  (r < 0) r = 0;
  
     var b = ((num >> 8) & 0x00FF) + amt;
- 
     if (b > 255) b = 255;
     else if  (b < 0) b = 0;
  
     var g = (num & 0x0000FF) + amt;
- 
     if (g > 255) g = 255;
     else if (g < 0) g = 0;
  
